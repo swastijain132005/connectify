e6e90e77b95646a480d15efa86f5c10a19c7b9eb
swasti <jainsanjna336@gmail.com>
2025-12-21 00:13:49 +0530

    Fix incorrect import

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import styles from "@/styles/Home.module.css";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   const Router = useRouter();
